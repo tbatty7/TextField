2909f5f13abd09d6b4c8fdbfffc706d1f83ea6b6
Mark <mark@industriallogic.com>
2022-02-11 11:57:54 -0500

    Add username and password fields with no spaces in username and navigation for return

--- TextField/ViewController.swift
+++ TextField/ViewController.swift
@@ -1,19 +1,41 @@
-//
-//  ViewController.swift
-//  TextField
-//
-//  Created by Timothy D Batty on 2/11/22.
-//
-
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
+    @IBOutlet private(set) var userNameField: UITextField!
+    @IBOutlet private(set) var passwordField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        if textField === userNameField {
+            return !string.contains(" ")
+        }
+        
+        return true
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        if textField === userNameField {
+            passwordField.becomeFirstResponder()
+        }
+        else {
+            guard let username = userNameField.text,
+                  let password = passwordField.text else {
+                      return false
+                  }
+            passwordField.resignFirstResponder()
+            performLogin(username: username, password: password)
+        }
+        
+        return false
+    }
 
-
+    private func performLogin(username: String, password: String) {
+        print("Username: \(username)  Password: \(password)")
+    }
 }
 
