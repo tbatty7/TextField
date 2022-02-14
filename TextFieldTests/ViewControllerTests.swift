1b69193be2c040da41afdd0007d680bec3ee2078
Tim Batty <tbatty7@gmail.com>
2022-02-14 11:50:20 -0500

    Added test for password field attributes

--- TextFieldTests/ViewControllerTests.swift
+++ TextFieldTests/ViewControllerTests.swift
@@ -2,25 +2,40 @@
 import XCTest
 
 final class ViewControllerTests: XCTestCase {
-
-    func test_outlets_shouldBeConnected() throws {
+    
+    private func setUpViewController() -> ViewController {
         let storyboard = UIStoryboard(name: "Main", bundle: nil)
         let viewController: ViewController = storyboard.instantiateViewController(identifier: String(describing: ViewController.self))
         viewController.loadViewIfNeeded()
+        
+        return viewController
+    }
+    
+    func test_outlets_shouldBeConnected() throws {
+        let viewController: ViewController = setUpViewController()
+        
         XCTAssertNotNil(viewController.userNameField, "username field")
         XCTAssertNotNil(viewController.passwordField, "password field")
         XCTAssertEqual(String(describing: ViewController.self), "ViewController")
     }
     
     func test_usernameField_attributesShouldBeSet() {
-        let storyboard = UIStoryboard(name: "Main", bundle: nil)
-        let viewController: ViewController = storyboard.instantiateViewController(identifier: String(describing: ViewController.self))
-        viewController.loadViewIfNeeded()
+        let viewController: ViewController = setUpViewController()
         
         let textField = viewController.userNameField!
         
         XCTAssertEqual(textField.textContentType, .username, "text field content type")
         XCTAssertEqual(textField.autocorrectionType, .no, "text field autocorrection type")
         XCTAssertEqual(textField.returnKeyType, .next, "text field return key type")
+    }
+    
+    func test_passwordField_attributesShouldBeSet() {
+        let viewController: ViewController = setUpViewController()
+        
+        let textField = viewController.passwordField!
+        
+        XCTAssertEqual(textField.textContentType, .password, "text field content type")
+        XCTAssertEqual(textField.returnKeyType, .go, "text field return key type")
+        XCTAssertTrue(textField.isSecureTextEntry, "text field isSecureTextEntry")
     }
 }
