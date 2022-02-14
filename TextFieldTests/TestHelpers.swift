4611cbf98b3e3ced13e6fb78980e482cf432a09b
Mark <mark@industriallogic.com>
2022-02-14 11:32:20 -0500

    Add better error description for UIReturnKeyType

--- TextFieldTests/TestHelpers.swift
+++ TextFieldTests/TestHelpers.swift
@@ -1,10 +1,3 @@
-//
-//  TestHelpers.swift
-//  TextFieldTests
-//
-//  Created by Timothy D Batty on 2/14/22.
-//
-
 import Foundation
 import UIKit
 
@@ -25,6 +18,39 @@ extension UITextAutocorrectionType: CustomStringConvertible {
             return "yes"
         @unknown default:
             fatalError("Unknown UITextAutocorrectionType")
+        }
+    }
+}
+
+extension UIReturnKeyType : CustomStringConvertible {
+    public var description: String {
+        switch self {
+        case .default:
+            return "default"
+        case .go:
+            return "go"
+        case .google:
+            return "google"
+        case .join:
+            return "join"
+        case .next:
+            return "next"
+        case .route:
+            return "route"
+        case .search:
+            return "search"
+        case .send:
+            return "send"
+        case .yahoo:
+            return "yahoo"
+        case .done:
+            return "done"
+        case .emergencyCall:
+            return "emergencyCall"
+        case .continue:
+            return "continue"
+        @unknown default:
+            fatalError("Unknown UIReturnKeyType")
         }
     }
 }
